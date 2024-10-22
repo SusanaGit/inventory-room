7079f4c1100400ea8e655d9b9f8265f89e1293d8
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-22 19:55:04 +0200

    starting testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,4 +85,7 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
